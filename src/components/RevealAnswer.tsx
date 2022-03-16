8fa9f33a854810eaa5ab4a3337ed7dc401b8444e
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-16 17:03:07 -0400

    some components

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    //const[state, setState] = useState<number>(intialvalue);
+    const [visible, setVisible] = useState<boolean>(false);
+    //this is the set Setter function
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
