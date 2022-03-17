14578275e703bbd4eecbeafdd84d7498f1e877ae
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-17 19:03:57 -0400

    double half

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+import { setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
+//deleted the import
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+//created an interface with the two attriubtes
+interface numInt {
+    dhValue: number;
+    setDhValue: (dhValue: number) => void;
 }
 
-function Halver(): JSX.Element {
+//passed in the arguments (the attriubtes from the interface)
+//{attriubte1, attriubte2}: interfaceName
+function Doubler({ dhValue, setDhValue }: numInt): JSX.Element {
+    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+}
+//passed in the arguments (the attriubtes from the interface)
+//{attriubte1, attriubte2}: interfaceName
+function Halver({ dhValue, setDhValue }: numInt): JSX.Element {
     return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    //useState function
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
+            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
         </div>
     );
 }
