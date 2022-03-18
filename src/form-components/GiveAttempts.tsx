2a2b72aaa7093372a312f9820b847ee2ffb31a39
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-18 01:47:35 -0400

    Give Attempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    //state representing the number of attempts the user has left
+    const [attempt, setLeft] = useState<number>(3);
+    //state representing the number requested
+    const [requested, updatedRequested] = useState<string>("0");
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="giveAttempts">
+                <Form.Label>Number of Attempts:</Form.Label>
+                <Form.Control
+                    type="numnber"
+                    value={requested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        updatedRequested(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>
+                <Button
+                    onClick={() => setLeft(attempt - 1)}
+                    disabled={attempt <= 0 ? true : false}
+                >
+                    Attempt Used
+                </Button>
+                <Button
+                    onClick={() =>
+                        setLeft(parseInt(requested) + attempt || attempt)
+                    }
+                >
+                    Attempt Added
+                </Button>
+            </div>
         </div>
     );
 }
