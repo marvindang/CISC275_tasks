53f9fa4300f0353c451464e75296a4b5d6458b27
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-18 20:46:26 -0400

    uploaded image

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    return (
+        <div>
+            <h3>Quizzer</h3>
+            <h3>Quizzer</h3>
+            <div>
+                <img
+                    src={
+                        "/Users/marvindang/CISC275_tasks/src/quizzer/IMG_6120.HEIC"
+                    }
+                    alt="Quizzer"
+                />
+            </div>
+        </div>
+    );
 }
