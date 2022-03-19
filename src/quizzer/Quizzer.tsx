269eb5612d6b1c7ddd26f45a6105a9d28024e02c
marvindang <67122144+marvindang@users.noreply.github.com>
2022-03-18 21:01:14 -0400

    created list

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 
 export function Quizzer(): JSX.Element {
+    const tasks: string[] = ["Sketch,"];
     return (
         <div>
             <h3>Quizzer</h3>
-            <h3>Quizzer</h3>
             <div>
                 <img
-                    src="/Users/marvindang/CISC275_tasks/src/quizzer/IMG_6120.HEIC"
+                    src={
+                        "/Users/marvindang/CISC275_tasks/src/quizzer/IMG_6120.HEIC"
+                    }
                     alt="Quizzer"
                 />
+            </div>
+            <div>
+                <ul>
+                    {tasks.map((name: string) => (
+                        <li key={name}>{name}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
